21586c2655c8a295902f833b8d870fda5e9fa594
vadym_makhonin <vadym_makhonin@epam.com>
2017-11-08 22:47:20 +0200

    APlusB task added

---
+++ Algorithmic Toolbox/Week 1/APlusB.js
@@ -0,0 +1,17 @@
+let readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+let rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', readLine);
+
+function readLine (line) {
+  if (line !== "\n") {
+    let a = parseInt(line.toString().split(' ')[0], 10);
+    let b = parseInt(line.toString().split(' ')[1], 10);
+    console.log(a + b);
+    process.exit();
+  }
+}
